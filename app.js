fee1e9ec69280d0eec4bdb1061caec244e604463
Igor Sergiienko <Igor_Sergiienko@epam.com>
2015-11-25 18:55:37 +0200

    trying to add some backgrounds2

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 var content = $('.content');
 
 $('.item').on('mouseenter', function(){
-	
+	var currentBg = $('.bg').eq(1),
+		nextBg = $('.bg').eq(0);
 
 
-
-
-	// var self = this;
-	// var oldBg = $('.bg');
-	// var newBg = $('.bg').clone();
-	// newBg.css('background-image', 'url(' + self.getAttribute('data-bg') + ')');
-	// newBg.prependTo('body');
-	// oldBg.fadeOut(function(){
-	// 			oldBg.remove();
-	// 		});
+	nextBg.css('background-image', 'url(' + this.getAttribute('data-bg') + ')');
+	currentBg.fadeOut();
+	nextBg.fadeIn(function(){
+		currentBg.hide().prependTo('body');
+	});
 });
+
+$('.bg').on('mouseenter', function(){
+	var currentBg = $('.bg').eq(1),
+		nextBg = $('.bg').eq(0);
+
+	if (currentBg.css('background-image') === 'url(file:///D:/Workspace/traveller/img/traveller.jpg)'){
+		return;
+	}
+
+	nextBg.css('background-image', 'url(img/traveller.jpg)');
+	currentBg.fadeOut();
+	nextBg.fadeIn(function(){
+		currentBg.hide().prependTo('body');
+	});
+});
+
+function changeBg(currentBg, nextBg, url){
+	url = url || 'url(img/traveller.jpg)';
+}
