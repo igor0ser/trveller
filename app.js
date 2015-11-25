448c58e1f26e3bfc1fd5a3bfafb5d76182adc2be
Igor Sergiienko <Igor_Sergiienko@epam.com>
2015-11-25 19:13:29 +0200

    trying to add some backgrounds3

--- app.js
+++ app.js
@@ -5,28 +5,37 @@ $('.item').on('mouseenter', function(){
 		nextBg = $('.bg').eq(0);
 
 
-	nextBg.css('background-image', 'url(' + this.getAttribute('data-bg') + ')');
-	currentBg.fadeOut();
-	nextBg.fadeIn(function(){
-		currentBg.hide().prependTo('body');
-	});
+	// nextBg.css('background-image', 'url(' + this.getAttribute('data-bg') + ')');
+	// currentBg.fadeOut();
+	// nextBg.fadeIn(function(){
+	// 	currentBg.hide().prependTo('body');
+	// });
+	url = this.getAttribute('data-bg');
+	changeBg(currentBg, nextBg, url);
 });
 
 $('.bg').on('mouseenter', function(){
-	var currentBg = $('.bg').eq(1),
-		nextBg = $('.bg').eq(0);
+	var currentBg = $('.bg').eq(1);
 
-	if (currentBg.css('background-image') === 'url(file:///D:/Workspace/traveller/img/traveller.jpg)'){
+	if (currentBg.css('background-image').includes('traveller.jpg') ){
+	console.log('ok');
 		return;
 	}
 
-	nextBg.css('background-image', 'url(img/traveller.jpg)');
+	var nextBg = $('.bg').eq(0);
+	// nextBg.css('background-image', 'url(img/traveller.jpg)');
+	// currentBg.fadeOut();
+	// nextBg.fadeIn(function(){
+	// 	currentBg.hide().prependTo('body');
+	// });
+	changeBg(currentBg, nextBg);
+});
+
+function changeBg(currentBg, nextBg, url){
+	url = url || 'img/traveller.jpg';
+	nextBg.css('background-image', 'url(' + url + ')');
 	currentBg.fadeOut();
 	nextBg.fadeIn(function(){
 		currentBg.hide().prependTo('body');
 	});
-});
-
-function changeBg(currentBg, nextBg, url){
-	url = url || 'url(img/traveller.jpg)';
 }
