65b011f6cf714c53db7a2d39b286312e6c28278d
Igor Sergiienko <Igor_Sergiienko@epam.com>
2015-11-25 18:27:33 +0200

    trying to add some backgrounds

---
+++ app.js
@@ -0,0 +1,17 @@
+var content = $('.content');
+
+$('.item').on('mouseenter', function(){
+	
+
+
+
+
+	// var self = this;
+	// var oldBg = $('.bg');
+	// var newBg = $('.bg').clone();
+	// newBg.css('background-image', 'url(' + self.getAttribute('data-bg') + ')');
+	// newBg.prependTo('body');
+	// oldBg.fadeOut(function(){
+	// 			oldBg.remove();
+	// 		});
+});
